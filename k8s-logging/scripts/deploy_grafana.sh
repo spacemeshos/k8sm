846261997e92f37e4032ac2929b39a15bcfb242e
y0sher <lyosher@gmail.com>
2019-12-18 11:18:56 +0200

    Added prometheus and grafana settings and services

--- k8s-logging/scripts/deploy_grafana.sh
+++ k8s-logging/scripts/deploy_grafana.sh
@@ -3,16 +3,17 @@
 set -x
 # output:
 
-helm install \
+helm install grafana \
       --set nodeSelector.role="infra" \
       --set service.type="NodePort" \
       --set persistence.enabled="true" \
       --set datasources."datasources\.yaml".apiVersion=1 \
       --set datasources."datasources\.yaml".datasources[0].name=Prometheus \
       --set datasources."datasources\.yaml".datasources[0].type=prometheus \
-      --set datasources."datasources\.yaml".datasources[0].url=http://prom.spacemesh.io \
+      --set datasources."datasources\.yaml".datasources[0].url=http://prometheus-server.monitor.svc.cluster.local:80 \
       --set datasources."datasources\.yaml".datasources[0].access=proxy \
       --set datasources."datasources\.yaml".datasources[0].isDefault=true \
-      --name grafana \
       --namespace monitor \
       stable/grafana
+
+kubectl apply -f grafana_service.yml -n monitor
--- k8s-logging/scripts/deploy_prometheus.sh
+++ k8s-logging/scripts/deploy_prometheus.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+
 set -x
 # output:
 
-helm install \
-      --set server.nodeSelector.role="infra" \
+helm install prometheus --set server.nodeSelector.role="infra" \
       --set server.service.type="NodePort" \
       --set pushgateway.nodeSelector.role="infra" \
       --set kubeStateMetrics.nodeSelector.role="infra" \
       --set alertmanager.nodeSelector.role="infra" \
       --set rbac.create=true \
-      --set server.persistentVolume.enabled=false \
+      --set server.persistentVolume.enabled=true \
+      --set server.persistentVolume.size="52Gi" \
+      --set server.resources.limits.cpu="3" \ # Todo we need more but our gcp machine type has only 4
+      --set server.resources.limits.memory="16Gi" \ # Todo we need more for heavy queries
       --set alertmanager.persistentVolume.enabled=false \
       --set alertmanager.enabled=false \
       --set kubeStateMetrics.enabled=false \
       --set nodeExporter.enabled=false \
       --set pushgateway.enabled=false \
-      --name prometheus \
+      --set server.retention="48h" \
       --namespace monitor \
       stable/prometheus
+
+kubectl apply -f prom-service.yml -n monitor
